6a961f895991507b335756ddecfb008e56f88423
Volodymyr Shtoda <30345665+JavaEEPRO@users.noreply.github.com>
2017-10-25 08:20:07 +0300

    cheer up! I have overcome the world
    I have told you these things, that in me you may have peace. In the world you have oppression; but cheer up! I have overcome the world. (John 16:33)

---
+++ thinkingInJava/synchronizedVolatileYild.java
@@ -0,0 +1,17 @@
+
+http://wikijava.it-cache.net/index.php@title=Glava_17_Thinking_in_Java_4th_edition.html#.D0.9A.D1.80.D0.B8.D1.82.D0.B8.D1.87.D0.B5.D1.81.D0.BA.D0.B8.D0.B5_.D1.81.D0.B5.D0.BA.D1.86.D0.B8.D0.B8
+
+//I have told you these things, that in me you may have peace. In the world you have oppression; 
+//but cheer up! I have overcome the world. (John 16:33)
+
+
+
+
+
+
+
+
+/*
+
+
+*/
